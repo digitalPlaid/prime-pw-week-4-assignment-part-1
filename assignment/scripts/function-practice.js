63c340612f70bde0e894346e8d8ae8e07b6684a7
Dan St. Aubin <staubindj@gmail.com>
2021-07-01 10:58:00 -0500

    added a function to do some testing and fin thru 7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -24,27 +24,31 @@ console.log(helloName('Jasmine'))
 function addNumbers( firstNumber, secondNumber ) {
   return firstNumber + secondNumber;
 }
-console.log('Testing addNumbers: ');
 
+// tests
 let num1 = 2;
 let num2 = 100;
 let expected = 102;
 let actual = addNumbers(num1,num2);
-let message = expected === actual ? 'Test passed!' : 'Test Failed!';
-console.log(message + ` Actual: ${actual}, Expected: ${expected}`);
+compareEqual('addNumbers',actual,expected)
+
+
+
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( num1, num2, num3 ){
   return num1*num2*num3;
 }
 
+// tests
 console.log('Testing multiplyThree');
 num1 = 2;
 num2 = 20;
 num3 = 30
 expected = 1200;
 actual = multiplyThree(num1,num2,num3);
-console.log(message + ` Actual: ${actual}, Expected: ${expected}`);
+compareEqual('multiplyThree',actual,expected)
+
 
 
 // 5. Function that will return true if a number is positive, 
@@ -68,25 +72,47 @@ function getLast( array ) {
   if (array.length === 0) return undefined;
   return array[array.length-1]
 }
-console.log('Testing getLast:');
-console.log('getLast populated array');
+
+// tests
 let someArray = [1,2,3,4,5];
 expected = 5;
 actual = getLast(someArray);
-console.log(message + ` Actual: ${actual}, Expected: ${expected}`);
+compareEqual('getLast',actual,expected, 'populated array');
 
-console.log('getLast empty array');
+
 someArray = [];
 expected = undefined;
 actual = getLast(someArray);
-console.log(message + ` Actual: ${actual}, Expected: ${expected}`);
+compareEqual('getLast',actual,expected, 'empty array');
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  for (element of array) {
+    if (value === element) return true;
+  }
+  return false;
 }
+
+
+// tests
+someArray = [1,2,3,4,5];
+target = 4
+expected = true;
+actual = find(target, someArray);
+compareEqual('find',actual,expected, 'target value in array')
+
+
+someArray = [];
+expected = undefined;
+actual = getLast(someArray);
+compareEqual('find',actual,expected, 'target value not present');
+
+
+
+
 
 // ----------------------
 // Stretch Goals
@@ -115,3 +141,16 @@ function sumAll( ) {
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+
+
+
+
+
+// function to test and write out some info that may be useful to the console.
+function compareEqual(functionNameAsStr, actual, expected, testCase='') {
+  console.log(`Testing ${functionNameAsStr}: `);
+  let message = expected === actual ? 'Test passed!' : 'Test Failed!';
+  if (testCase) console.log(`Testing case: ${testCase}`)
+  console.log(message + ` Actual: ${actual}, Expected: ${expected}`);
+}
